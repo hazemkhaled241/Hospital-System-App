9a8c2a5f581888572ed4da89c9b96610a0888a29
hazemkhaled241 <84819643+hazemkhaled241@users.noreply.github.com>
2022-03-13 22:59:32 +0200

    MedicalMeasurementManagerFragment is completely done

---
+++ app/src/main/java/com/example/medical/Manager/MedicalMeasurementManagerFragment.java
@@ -0,0 +1,23 @@
+package com.example.medical.Manager;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.medical.R;
+
+
+public class MedicalMeasurementManagerFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_medical_measurment_manager, container, false);
+    }
+}
